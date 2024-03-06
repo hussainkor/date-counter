b04edcd2dbba5383eee0c8bcef2ecbe04ae27df1
hussainkor <hussainkor@gmail.com>
2024-03-06 22:24:10 +0530

    pushed inside the changes of App.js page and Index.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Date Counter</h2>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const stepIncrement = () => setStep((s) => s + 1);
+  const stepDecrement = () => {
+    if (step > 1) {
+      setStep((s) => s - 1);
+    }
+  }
+  const incrementCount = () => setCount((c) => c + step);
+  const decrementCount = () => setCount((c) => c - step);
+  return (
+    <div className="date-counter">
+      <div className="steps">
+        <button onClick={stepDecrement}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={stepIncrement}>+</button>
+      </div>
+      <div className="date">
+        <DateChanger count={count} />
+      </div>
+      <div className="counter">
+        <button onClick={decrementCount}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={incrementCount}>+</button>
+      </div>
+    </div>
+  );
+}
+
+function DateChanger(props) {
+  const count = props.count;
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  const checkDate =
+    count > 0 ? `${count} days from today is` : `${count} days ago was`;
+
+  const dater = count > 0 || count < 0 ? `${checkDate}` : "Today is";
+
+  return (
+    <div>
+      <p>
+        {dater} {date.toDateString()}
+      </p>
+    </div>
+  );
+}
